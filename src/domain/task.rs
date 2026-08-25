1ce7c8862e10e2d83cf67cd117e0e92f33a5d08c
staylazy <semenkondratieff@gmail.com>
2026-08-25 20:35:50 +0300

    feat: storage delete/get

--- src/domain/task.rs
+++ src/domain/task.rs
@@ -1,5 +1,10 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Task {
     pub id: u64,
     pub title: String,
     pub description: String,
 }
+
+// TODO: add DTO task in storage/dto.rs TaskDto with Serialize/Deserialize
--- src/storage/repository.rs
+++ src/storage/repository.rs
@@ -1,6 +1,20 @@
 use crate::domain::Task;
 
-pub trait TaskRepository {
-    fn save(&mut self, task: &Task);
-    fn test(&mut self);
+
+#[derive(Debug)]
+pub enum RepositoryError {
+    Unavailable, 
+    Corrupted,
+    Internal
 }
+    
+pub trait TaskRepository {
+    fn save(&mut self, task: &Task) -> Result<(), RepositoryError>;
+
+    fn get(&self, id: u64) -> Result<Option<Task>, RepositoryError>;
+
+    fn get_all(&self) -> Result<Vec<Task>, RepositoryError>;
+
+    fn delete(&self, id: u64) -> Result<(), RepositoryError>;
+}
+
