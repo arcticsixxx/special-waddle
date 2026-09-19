0dd180d2b8d85e64f1c5cc0c528caf1d0b91d536
Efim Denisov <e.denisov@movavi.com>
2026-09-19 23:46:46 +0200

    add ui input

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,84 @@
+use crossterm::event::{self, KeyCode, KeyEventKind};
 use special_waddle::{
     app,
     parser::{self, handler},
     storage,
+    tui::app::{
+        Direction::{Left, Right},
+        InputMode, UserInput,
+    },
 };
 
 use tokio::sync::mpsc;
 
-#[tokio::main]
-async fn main() {
-    let file_repo = storage::file::FileTaskRepository::new("storage.json");
-    let mut service =
-        app::task_service::TaskService::new(file_repo).expect("Failed to create task service");
+// #[tokio::main]
+// async fn main() {
+//     let file_repo = storage::file::FileTaskRepository::new("storage.json");
+//     let mut service =
+//         app::task_service::TaskService::new(file_repo).expect("Failed to create task service");
 
-    let (tx, mut rx) = mpsc::channel(32);
+//     let (tx, mut rx) = mpsc::channel(32);
 
-    tokio::spawn(async move {
-        loop {
-            match parser::parser::input::<storage::file::FileTaskRepository>() {
-                Ok(cmd) => tx.send(cmd).await.unwrap(),
-                Err(e) => println!("Error occured {}", e),
-            }
-        }
-    });
+//     tokio::spawn(async move {
+//         loop {
+//             match parser::parser::input::<storage::file::FileTaskRepository>() {
+//                 Ok(cmd) => tx.send(cmd).await.unwrap(),
+//                 Err(e) => println!("Error occured {}", e),
+//             }
+//         }
+//     });
 
-    while let Some(action) = rx.recv().await {
-        handler::handle_cli(action, &mut service);
-    }
-}
+//     while let Some(action) = rx.recv().await {
+//         handler::handle_cli(action, &mut service);
+//     }
+// }
 
-// use special_waddle::tui::app::TuiApp;
-// use special_waddle::tui::ui;
+use special_waddle::tui::app::TuiApp;
+use special_waddle::tui::ui;
 // use special_waddle::{app, storage};
 
-// fn main() {
-//     let file_repo = storage::file::FileTaskRepository::new("storage.json");
-//     let service =
-//         app::task_service::TaskService::new(file_repo).expect("Failed to create task service");
-//     let state = TuiApp {
-//         tasks: service.tasks().unwrap(),
-//         active_task: service.active_task().unwrap(),
-//         should_exit: false,
-//     };
+fn run_tui(state: &mut TuiApp) {
+    ratatui::run(|terminal| {
+        while !state.should_exit {
+            terminal.draw(|frame| ui::render(frame, &state)).unwrap();
 
-//     ratatui::run(|terminal| {
-//         while !state.should_exit {
-//             terminal.draw(|frame| ui::render(frame, &state)).unwrap();
-//         }
-//     })
-// }
+            if let Some(key) = event::read().unwrap().as_key_press_event() {
+                match state.user_input.input_mode {
+                    InputMode::Normal => match key.code {
+                        KeyCode::Char('e') => {
+                            state.user_input.input_mode = InputMode::Editing;
+                        }
+                        KeyCode::Char('q') => {
+                            state.should_exit = true;
+                        }
+                        _ => {}
+                    },
+                    InputMode::Editing if key.kind == KeyEventKind::Press => match key.code {
+                        KeyCode::Enter => state.user_input.submit_input(),
+                        KeyCode::Char(to_insert) => state.user_input.enter_char(to_insert),
+                        KeyCode::Backspace => state.user_input.delete_char(),
+                        KeyCode::Left => state.user_input.move_cursor(Left),
+                        KeyCode::Right => state.user_input.move_cursor(Right),
+                        KeyCode::Esc => state.user_input.input_mode = InputMode::Normal,
+                        _ => {}
+                    },
+                    InputMode::Editing => {}
+                }
+            }
+        }
+    })
+}
+
+fn main() {
+    let file_repo = storage::file::FileTaskRepository::new("storage.json");
+    let service =
+        app::task_service::TaskService::new(file_repo).expect("Failed to create task service");
+    let mut state = TuiApp {
+        tasks: service.tasks().unwrap(),
+        active_task: service.active_task().unwrap(),
+        should_exit: false,
+        user_input: UserInput::new(),
+    };
+
+    run_tui(&mut state);
+}
--- src/tui/ui.rs
+++ src/tui/ui.rs
@@ -1,13 +1,21 @@
+use std::process::exit;
+
 use ratatui::{
     Frame,
     layout::{Constraint, Layout},
     widgets::{Block, List, ListItem, Paragraph},
 };
+use tokio::task;
 
 use crate::tui::app::TuiApp;
 
 pub fn render(frame: &mut Frame, app: &TuiApp) {
-    let areas = Layout::vertical([Constraint::Min(3), Constraint::Length(3)]).split(frame.area());
+    let layout = Layout::vertical([
+        Constraint::Min(3),
+        Constraint::Length(3),
+        Constraint::Length(3),
+    ]);
+    let [task_list_area, active_task_area, input_area] = frame.area().layout(&layout);
 
     let items: Vec<ListItem> = app
         .tasks
@@ -17,7 +25,7 @@ pub fn render(frame: &mut Frame, app: &TuiApp) {
 
     let list = List::new(items).block(Block::bordered());
 
-    frame.render_widget(list, areas[0]);
+    frame.render_widget(list, task_list_area);
 
     let active_task = match &app.active_task {
         Some(task) => {
@@ -28,5 +36,38 @@ pub fn render(frame: &mut Frame, app: &TuiApp) {
 
     let active_item = Paragraph::new(active_task).block(Block::bordered());
 
-    frame.render_widget(active_item, areas[1]);
+    frame.render_widget(active_item, active_task_area);
+
+    render_input(frame, app, input_area);
+}
+
+pub fn render_input(frame: &mut Frame, app: &TuiApp, input_area: ratatui::layout::Rect) {
+    let input = Paragraph::new(app.user_input.input.as_str())
+        .style(match app.user_input.input_mode {
+            super::app::InputMode::Normal => ratatui::style::Style::default(),
+            super::app::InputMode::Editing => {
+                ratatui::style::Style::default().fg(ratatui::style::Color::Yellow)
+            }
+        })
+        .block(Block::bordered());
+
+    frame.render_widget(input, input_area);
+
+    match app.user_input.input_mode {
+        // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
+        super::app::InputMode::Normal => {}
+
+        // Make the cursor visible and ask ratatui to put it at the specified coordinates after
+        // rendering
+        #[expect(clippy::cast_possible_truncation)]
+        super::app::InputMode::Editing => {
+            frame.set_cursor_position(ratatui::layout::Position::new(
+                // Draw the cursor at the current position in the input field.
+                // This position can be controlled via the left and right arrow key
+                input_area.x + app.user_input.character_index as u16 + 1,
+                // Move one line down, from the border to the input line
+                input_area.y + 1,
+            ))
+        }
+    }
 }
